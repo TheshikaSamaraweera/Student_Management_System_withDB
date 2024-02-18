c877baaa3c6fb85f93b7e8249a8af45e86bed1db
Theshikanavod <theshikanavod@gmail.com>
2024-02-18 09:02:13 +0530

    commit 1
    completed

---
+++ Student_Management_System/Views/ResultsWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Student_Management_System.ViewModels;
+using System.Windows;
+
+
+namespace Student_Management_System.Views
+{
+    /// <summary>
+    /// Interaction logic for ResultsWindow.xaml
+    /// </summary>
+    public partial class ResultsWindow : Window
+    {
+        public ResultsWindow(ResultsViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged_1(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged_2(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Student_Management_System/Views/StudentWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Student_Management_System.ViewModels;
+using System.Windows;
+
+namespace Student_Management_System.Views
+{
+    /// <summary>
+    /// Interaction logic for StudentWindow.xaml
+    /// </summary>
+    public partial class StudentWindow : Window
+    {
+        public StudentWindow()
+        {
+            InitializeComponent();
+            DataContext = new StudentWindowViewModel();
+        }
+
+        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
